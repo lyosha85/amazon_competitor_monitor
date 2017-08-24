e9f0cee6233657cfbbb91fda054cc93fcdcd9891
Alex <lyosha85@gmail.com>
2017-08-24 13:33:00 -0700

    Validate account name presence and update todo list

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation' do
+    describe 'name' do
+      let(:account) { FactoryGirl.create(:account) }
+      it 'should be present' do
+        account.name = ''
+        expect(account.valid?).to be false
+      end
+    end
+  end
 end
