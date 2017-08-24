d91e7c360193d8c80d72ac2950728196ed1d450f
Alex <lyosha85@gmail.com>
2017-08-24 15:24:23 -0700

    Add spec for max 10 groups per account

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Group, type: :model do
   context 'validation' do
-    let(:group) { FactoryGirl.build(:group) }
+    let(:account) { FactoryGirl.create(:account) }
+    let(:group)   { FactoryGirl.create(:group, account: account) }
 
     describe 'name' do
       it 'should be present' do
@@ -14,6 +15,12 @@ RSpec.describe Group, type: :model do
       it 'should be present' do
         group.account = nil
         expect(group.valid?).to be false
+      end
+    end
+    describe 'groups per account' do
+      it 'max 10' do
+         11.times { Group.create(account: account, name: "Group") }
+          expect(account.groups.count).to eq(10)
       end
     end
   end
