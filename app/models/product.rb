29760896b6c74876add0ec7d33d98a6a2d728879
Alex <lyosha85@gmail.com>
2017-08-24 15:44:59 -0700

    Validate max 8 products per category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :group
+
   validates_presence_of :asin
+  validate :check_max_8_products_per_group
+
+  def check_max_8_products_per_group
+    if self.group
+      errors.add :group, 'Max 8 products per group.' if self.group.products.count >= 8
+    end
+  end
 end
