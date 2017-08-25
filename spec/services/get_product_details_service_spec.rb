508df7873db2ada19478c12886b6530e10bc869b
Alex <lyosha85@gmail.com>
2017-08-25 12:53:53 -0700

    Get inventory count

--- spec/services/get_product_details_service_spec.rb
+++ spec/services/get_product_details_service_spec.rb
@@ -5,14 +5,18 @@ RSpec.describe GetProductDetailsService do
   subject { service = GetProductDetailsService.new(asin) }
 
   it 'returns product price', :vcr do
-    expect(subject.send(:price_cents)).to eq '599'
+    expect(subject.send(:price_cents)).to be_present
   end
 
   it 'returns product title', :vcr do
-    expect(subject.send(:title)).to eq 'Mostsola 20 Pcs LED Photo Clip String Lights Perfect for Pictures Notes Artwork LED Decor (Warm White)'
+    expect(subject.send(:title)).to be_present
   end
 
   it 'returns product best seller rank number', :vcr do
-    expect(subject.send(:bsr)).to be_a_kind_of(Integer)
+    expect(subject.send(:bsr)).to be_present
+  end
+
+  it 'returns product inventory count', :vcr do
+    expect(subject.send(:inventory)).to be_present
   end
 end
