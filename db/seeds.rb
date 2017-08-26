35225fdf4cb8756411ea5ea2b91d9b90e1f7ba37
Alex <lyosha85@gmail.com>
2017-08-26 04:50:26 -0700

    Add inventory, remove description from snapshots table and add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+account =  FactoryGirl.create(:account, name: 'Alex')
+group = FactoryGirl.create(:group, name: 'My Competitors', account: account)
+
+%w(B00ZLJ1QGC B01K8B5BYU B01JP6E9HY B011BX2C50 B002BZIVSU B00L9A95T2).each do |asin|
+  FactoryGirl.create(:product, group: group, asin: asin)
+end
