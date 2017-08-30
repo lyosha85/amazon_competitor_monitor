22e0d43a9941b35f171fc792ba3117d81dadd5c9
Alex <lyosha85@gmail.com>
2017-08-30 04:08:35 -0700

    Show changes page

--- app/models/snapshot.rb
+++ app/models/snapshot.rb
@@ -1,3 +1,8 @@
 class Snapshot < ApplicationRecord
   validates_presence_of :asin
+
+  def previous_snapshot # with the same asin
+    Snapshot.where(asin: self.asin)
+            .where("id < ?", id).last
+  end
 end
