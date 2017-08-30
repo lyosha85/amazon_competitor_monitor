f7e382f4c9eaecc8a064e7afb63e8c09eb0cae4a
Alex <lyosha85@gmail.com>
2017-08-29 23:22:56 -0700

    Refactor product snapshot worker

--- app/workers/products_snapshots_worker.rb
+++ app/workers/products_snapshots_worker.rb
@@ -8,16 +8,12 @@ class ProductsSnapshotsWorker
     Rails.logger.info "Attempting to get data of #{asin}."
 
     snapshot_attributes = GetProductDetailsService.call(asin)
-    snapshot = Snapshot.new(snapshot_attributes)
-    new_snapshot_attributes = snapshot.attributes.except('id', 'created_at',
-                                                         'updated_at')
-    previous_snapshot_attributes = Snapshot.where(asin: asin).last
-                                           .attributes.except('id',
-                                                              'created_at',
-                                                              'updated_at')
+    last_snapshot = Snapshot.where(asin: asin).last
+    last_snapshot.assign_attributes(snapshot_attributes)
+
     # Only create a snapshot if something changed
-    unless snapshot_attributes == previous_snapshot_attributes
-      snapshot.save!
+    if last_snapshot.changed?
+      Snapshot.create!(snapshot_attributes)
       Product.where(asin: asin).each{|p| p.update(last_checked: Time.zone.now)}
     else
       Rails.logger.info "#{asin} remained the same."
